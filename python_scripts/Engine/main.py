178f58570b127c43bfb495db5097e509e9aa1e05
Anders Horgen Åsebø <andera15@uia.no>
2017-08-28 15:32:56 +0200

    Created Python GameEngine with a player, an enemy and support for Tiled background

--- python_scripts/Engine/main.py
+++ python_scripts/Engine/main.py
@@ -1,35 +1,27 @@
 import pygame
 import pygame.locals
+import Tile as tile
+import GameObject
+import KeyInput
+import random
 
-
-class GameObject():
-    vel = [0, 0]
-    pos = [100, 100]
-    orientation = 0
-    mov_animation = 0
-    running = False
-
-class Tile():
-    pos = []
-    obstacle = False
-    col = []
-
-
-
-p = GameObject()
+p = GameObject.GameObject()
+p.__init__()
 t = []
+bat = GameObject.GameObject()
+bat.__init__()
 
 def generate_tiles(w,h, list):
     for x in range(0, w):
         tx = []
         for y in range(0, h):
-            tx.append(Tile())
+            tx.append(tile.Tile())
             tx[y].pos = [x*32,y*32]
-            tx[y].col = [255,255,255]
+            tx[y].col = [random.randint(0,255),random.randint(0,255),random.randint(0,255)]
         list.append(tx)
 
 def load_tileset(filename, width, height, scale):
-    image = pygame.image.load(filename).convert()
+    image = pygame.image.load(filename).convert_alpha()
     image = pygame.transform.scale(image, (int(image.get_width()*scale), int(image.get_height()*scale)))
     image_width = image.get_width()
     image_height = image.get_height()
@@ -40,7 +32,7 @@ def load_tileset(filename, width, height, scale):
         for tile_y in range(0, image_height // int(height * scale)):
             rect = (tile_x * int(width * scale), tile_y * int(height * scale), int(width * scale), int(height * scale))
             line.append(image.subsurface(rect))
-    #print(len(tile_table[7]))
+    #print(len(tile_table))
     return tile_table
 
 def movement():
@@ -54,31 +46,26 @@ def movement():
         p.vel[0] = 3
     p.vel[0] = 0 if not keys[1] and not keys[3] else p.vel[0]
     p.vel[1] = 0 if not keys[0] and not keys[2] else p.vel[1]
-    p.pos[0] += p.vel[0]
-    p.pos[1] += p.vel[1]
+    #p.pos[0] += p.vel[0]
+    #p.pos[1] += p.vel[1]
 
-def player_orientation():
-    if (p.vel[0] > 0):
-        p.orientation = 1
-    elif (p.vel[0] < 0):
-        p.orientation = 0
-    if (p.vel[1] > 0):
-        p.orientation = 3
-    elif (p.vel[1] < 0):
-        p.orientation = 2
+def object_orientation(obj):
+    if (obj.vel[0] > 0):
+        obj.orientation = obj.right
+    elif (obj.vel[0] < 0):
+        obj.orientation = obj.left
+    if (obj.vel[1] > 0):
+        obj.orientation = obj.down
+    elif (obj.vel[1] < 0):
+        obj.orientation = obj.up
 
-def object_animation(obj):
-    obj.mov_animation += 1
-    if obj.mov_animation > 7 : obj.mov_animation = 0
-    if obj.vel[1] == 0 and obj.vel[0] == 0:
-        if obj.orientation == 0:
-            obj.mov_animation = 4
-        elif obj.orientation == 1:
-            obj.mov_animation = 0
-        elif obj.orientation == 2:
-            obj.mov_animation = 0
-        elif obj.orientation == 3:
-            obj.mov_animation = 5
+def object_animation(obj,time):
+    if obj.time >= time:
+        obj.time = 0
+        if obj.vel[1] != 0 or obj.vel[0] != 0 or obj.running:
+            obj.mov_animation += 1
+            if obj.mov_animation == len(obj.img) : obj.mov_animation = 0
+
 
 
 if __name__ == '__main__':
@@ -89,23 +76,41 @@ if __name__ == '__main__':
     playtime = 0.0
     FPS = 60
     gameloop = True
-    anim = 0
+    time = 0
     screen = pygame.display.set_mode((width, height))
+
+    #############LOAD IMAGE TILESET FOR GAMEOBJECT #############
+
     p.img = load_tileset('link.jpg', 64, 96, 0.5)
-    keys = [False, False, False, False]
+    bat.img = load_tileset('bat.png', 32,32,1.5)
+
+    ############################################################
+    bat.running = True
+    p.up, p.down, p.left, p.right = 2,3,0,1
+    bat.up, bat.down, bat.left, bat.right = 2, 0, 3, 1
+    keys = [False, False, False, False]         #List of which movement key is pressed
+
     generate_tiles(width//32,height // 32, t)
     while gameloop:
         milliseconds = clock.tick(FPS)
         playtime += milliseconds / 1000.0
+
         # Print framerate and playtime in titlebar.
         text = "FPS: {0:.2f}   Playtime: {1:.2f}".format(clock.get_fps(), playtime)
         pygame.display.set_caption(text)
-        #clear the screen before drawing it again
+
+        #Drawing all the tiles
         for i in t:
             for j in i:
                 pygame.draw.rect(screen, (j.col[0], j.col[1], j.col[2]), (j.pos[0], j.pos[1], 32, 32))
-        #draw the screen elements
+
+        ################### DRAW THE SCREEN ELEMENTS #################
+
         screen.blit(p.img[p.mov_animation][p.orientation], p.pos)
+        screen.blit(bat.img[bat.mov_animation][bat.orientation], bat.pos)
+
+        ############################################################
+
         #update the screen
         pygame.display.flip()
         # loop through the events
@@ -115,28 +120,21 @@ if __name__ == '__main__':
                 # if it is quit the game
                 pygame.quit()
                 exit(0)
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_w:
-                    keys[0] = True
-                elif event.key == pygame.K_a:
-                    keys[1] = True
-                elif event.key == pygame.K_s:
-                    keys[2] = True
-                elif event.key ==pygame. K_d:
-                    keys[3] = True
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_w:
-                    keys[0] = False
-                elif event.key == pygame.K_a:
-                    keys[1] = False
-                elif event.key == pygame.K_s:
-                    keys[2] = False
-                elif event.key == pygame.K_d:
-                    keys[3] = False
+
+            KeyInput.KeyCheck(event,keys) #Key events
         movement()
 
-        player_orientation()
-        anim += milliseconds
-        if(anim >= 80):
-            object_animation(p)
-            anim = 0
+        ######## CALL UPDATE FUNCTION FOR GAMEOBJECTS ########
+
+        p.update()
+        bat.update()
+
+        ######################################################
+
+        object_orientation(p)
+        object_orientation(bat)
+
+        p.time +=milliseconds
+        bat.time +=milliseconds
+        object_animation(p, 80)
+        object_animation(bat, 130)
